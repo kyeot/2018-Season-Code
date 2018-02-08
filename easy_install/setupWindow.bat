45271d00db0ff9c2708c8eebcf071044b5673f76
Cole Gerdemann <corvettecole@gmail.com>
2018-02-08 18:36:55 -0500

    slight improvement of installation batch script

--- easy_install/setupWindow.bat
+++ easy_install/setupWindow.bat
@@ -1,5 +1,3 @@
 SET runDir=%~dp0
-echo %runDir:~0,-1%
-xcopy /s %runDir%\libs %UserProfile%\wpilib\user\java\lib\
-echo "Installation complete"
+xcopy /s %runDir%libs %UserProfile%\wpilib\user\java\lib\
 pause
