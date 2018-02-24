635aa67940f72a17f3e5f638fdbe773954df3346
JacksonPass <jackson.pass@gmail.com>
2018-02-24 12:50:57 -0500

    Revised Control Scheme
    Changed control scheme from joystick control to trigger control

--- src/org/usfirst/frc2783/commands/TankDrive.java
+++ src/org/usfirst/frc2783/commands/TankDrive.java
@@ -32,20 +32,75 @@ public class TankDrive extends Command {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
+	public double averageWheelOutput(double lTrigger, double rTrigger) {
+		return rTrigger - lTrigger;
+	}
+	
+	public boolean isNegative(double value) {
+		return value < 0;
+	}
+	
+	public double scaleSide(char side, double initialOutput, double angularValue) {
+		boolean biggerRight = isNegative(angularValue);
+		boolean goingForward = !isNegative(angularValue);
+		if (goingForward) {
+			if (biggerRight) {
+				if (side == 'r') {
+					return initialOutput;
+				} else {
+					return (initialOutput + initialOutput*angularValue);
+				}
+			} else {
+				if (side == 'r') {
+					return initialOutput - initialOutput*angularValue;
+				} else {
+					return initialOutput;
+				}
+			}
+		} else {
+			if (biggerRight) {
+				if (side == 'r') {
+					return initialOutput;
+				} else {
+					return (initialOutput - initialOutput*angularValue);
+				}
+			} else {
+				if (side == 'r') {
+					return initialOutput + initialOutput*angularValue;
+				} else {
+					return initialOutput;
+				}
+			}
+		}
+	}
+	
+	public void setSpeeds(double scale) {
+		leftSpeed = scale*scaleSide('l', averageWheelOutput(OI.driver.getRawAxis(2), OI.driver.getRawAxis(3)), OI.driver.getRawAxis(0));
+		rightSpeed = scale*scaleSide('r', averageWheelOutput(OI.driver.getRawAxis(2), OI.driver.getRawAxis(3)), OI.driver.getRawAxis(0));
+	}
+	 
+	double leftSpeed;
+	double rightSpeed;
+	
 	protected void execute() {
-		double leftSpeed = OI.driver1.getRawAxis(1);
-		double rightSpeed = OI.driver2.getRawAxis(1);
-
-		if (OI.driver1.getRawButton(1)) {
-			leftSpeed = leftSpeed / 2;
-			rightSpeed = rightSpeed / 2;
+//		leftSpeed = OI.driver.getRawAxis(1);
+//		rightSpeed = OI.driver.getRawAxis(5);
+		double scale;
+		
+		if (OI.driver.getRawButton(5)) {
+			scale = .25;
+		} 
+		
+		else if (OI.driver.getRawButton(6)) {
+			scale = 1;
+		} 
+		
+		else {
+			scale = .5;
 		}
-
-		else if (OI.driver2.getRawButton(1)) {
-			leftSpeed = leftSpeed * 2;
-			rightSpeed = rightSpeed * 2;
-		}
-
+		
+		setSpeeds(scale);
+		
 		if (Math.abs(leftSpeed) < 0.15) {
 			leftSpeed = 0;
 		}
@@ -54,22 +109,22 @@ public class TankDrive extends Command {
 			rightSpeed = 0;
 		}
 
-	//	if (OI.driver.getRawButton(4)) {
-		//	navSensor.resetGyroNorth(0, 0);
-//		}
+		if (OI.driver.getRawButton(4)) {
+			navSensor.resetGyroNorth(0, 0);
+		}
 
-		//if (OI.driver.getRawButton(2)) {
-		//	if (fieldTransform.targetHistory.getLatestTarget() != null) {
-			//	angle = fieldTransform.targetHistory.getSmoothTarget().dir().getTheta();
-			//}
-	//	}
+		if (OI.driver.getRawButton(2)) {
+			if (fieldTransform.targetHistory.getLatestTarget() != null) {
+				angle = fieldTransform.targetHistory.getSmoothTarget().dir().getTheta();
+			}
+		}
 
-//	//	if (OI.driver.getRawButton(1)) {
-//			// Robot.tankDrive.setRobotPose(new Bearing(0));
-//			Robot.tankDrive.setRobotPose(new Bearing(angle));
-//		} else {
-//			Robot.tankDrive.tankDrive(leftSpeed, rightSpeed);
-//		}
+		if (OI.driver.getRawButton(1)) {
+			// Robot.tankDrive.setRobotPose(new Bearing(0));
+			Robot.tankDrive.setRobotPose(new Bearing(angle));
+		} else {
+			Robot.tankDrive.tankDrive(leftSpeed, rightSpeed);
+		}
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
