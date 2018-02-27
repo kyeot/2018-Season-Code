b9049c3c1f6530858531a83e67e2d5516428cea4
liaminatorshearin49 <lgshearin@yahoo.com>
2018-02-26 20:07:42 -0500

    Fixed Classes

--- src/org/usfirst/frc2783/segments/LeftSwitchToRightStage.java
+++ src/org/usfirst/frc2783/segments/LeftSwitchToRightStage.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc2783.segments;
+import java.util.ArrayList;
 
+import org.usfirst.frc2783.autonomous.StaticSetpoints;
+import org.usfirst.frc2783.autonomous.paths.Path;
+import org.usfirst.frc2783.autonomous.paths.PathBuilder.Waypoint;
+import org.usfirst.frc2783.autonomous.paths.PathContainer;
+import org.usfirst.frc2783.calculation.RigidTransform2d;
 public class LeftSwitchToRightStage {
 
 }
