14e36d39a951a1df88d79052a097f85475f2c299
Owenatkins29 <owenatkins39@yahoo.com>
2018-03-05 21:55:42 -0500

    Made ScaleFromRight Match
    Made ScaleFromRight Match

--- src/org/usfirst/frc2783/autonomous/actions/groups/ScaleFromRight.java
+++ src/org/usfirst/frc2783/autonomous/actions/groups/ScaleFromRight.java
@@ -5,6 +5,7 @@ import org.usfirst.frc2783.autonomous.actions.AutoDrive;
 import org.usfirst.frc2783.autonomous.actions.AutoElevator;
 import org.usfirst.frc2783.autonomous.actions.AutoIntake;
 import org.usfirst.frc2783.autonomous.actions.DriveWithGyroAndByDistance;
+import org.usfirst.frc2783.autonomous.actions.GoToTopAction;
 import org.usfirst.frc2783.autonomous.actions.RotateTankAction;
 import org.usfirst.frc2783.robot.Robot;
 import org.usfirst.frc2783.util.Bearing;
@@ -17,21 +18,21 @@ public class ScaleFromRight extends ActionGroup{
 		
 		try{
 			if(!Robot.isScaleLeft()){
-				addAction(new DriveWithGyroAndByDistance(0.5, 24.5*12, 24.5*12, 0));
+				addAction(new DriveWithGyroAndByDistance(0.5, 25*12, 25*12, 0));
 				addAction(new RotateTankAction(new Bearing(90), 1));
+				addAction(new AutoElevator(-0.5, 0.5));
 				addAction(new AutoDrive(-0.25, -0.25, 0.8));
-				addAction(new AutoElevator(-1, 2.7));
+				addAction(new GoToTopAction());
 				addAction(new AutoDrive(0.25, 0.25, 1.2));
 				addAction(new AutoIntake(0.75, 1));
 			}
 			else{
 				addAction(new DriveWithGyroAndByDistance(0.5, 18.8*12, 18.8*12, 0));
 				addAction(new RotateTankAction(new Bearing(89), 1));
-				addAction(new DriveWithGyroAndByDistance(0.5, 38.5*12, 38.5*12, 270));
-				addAction(new AutoDrive(0.5, 0, 0.5));
+				addAction(new DriveWithGyroAndByDistance(0.5, 36*12, 36*12, 270));
 				addAction(new RotateTankAction(new Bearing(0), 1));
-				addAction(new AutoElevator(-1, 2.7));
-				addAction(new AutoDrive(0.3, 0.3, 3.5));
+				addAction(new GoToTopAction());
+				addAction(new AutoDrive(0.3, 0.3, 2));
 				addAction(new AutoIntake(0.75, 1));
 			}
 			
