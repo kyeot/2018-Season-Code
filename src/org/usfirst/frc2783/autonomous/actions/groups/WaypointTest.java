e7d580e1a2c8ae8c54cca7641a61f27f8ebcde99
JacksonPass <jackson.pass@gmail.com>
2018-04-03 12:15:06 -0400

    Adjusted WaypointTest
    Changed WaypointTest to test path with curves (and it works). UNLIMITED POWER

--- src/org/usfirst/frc2783/autonomous/actions/groups/WaypointTest.java
+++ src/org/usfirst/frc2783/autonomous/actions/groups/WaypointTest.java
@@ -3,12 +3,13 @@ package org.usfirst.frc2783.autonomous.actions.groups;
 import org.usfirst.frc2783.autonomous.actions.ActionGroup;
 import org.usfirst.frc2783.autonomous.actions.DrivePathAction;
 import org.usfirst.frc2783.autonomous.paths.PathContainer;
-import org.usfirst.frc2783.autonomous.paths.toNodeAndBackTest;
+import org.usfirst.frc2783.segments.LeftStartToRightScale;
+import org.usfirst.frc2783.segments.StageLeft;
 
 public class WaypointTest extends ActionGroup {
 	public WaypointTest() {
 		super();
-		PathContainer testPath = new toNodeAndBackTest();
-		addAction(new DrivePathAction(testPath));
+		PathContainer testPath = new StageLeft();
+		addAction(new DrivePathAction(new LeftStartToRightScale()));
 	}
 }
