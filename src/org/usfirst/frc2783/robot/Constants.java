b4ab53bbded71154f1cec21815fc21c2bd69438a
Owenatkins29 <owenatkins39@yahoo.com>
2018-04-02 20:40:00 -0400

    Fixed Liam's Merge Error in Constants
    Fixed Liam's Merge Error in Constants

--- src/org/usfirst/frc2783/robot/Constants.java
+++ src/org/usfirst/frc2783/robot/Constants.java
@@ -47,19 +47,11 @@ public class Constants {
 	public static final int kSlowModeID = 5;
 	public static final int kFastModeID = 6;
 
-	// Rotator ID's	
-<<<<<<< HEAD
-	public static final int kFaceDriverStation= 1;
-//	public static final int kFaceRight = 2;
-//	public static final int kFaceLeft = 3;
+	public static final int kFaceDriverStation = 1;
 	public static final int kFaceDownField = 4;
 	
-	public static final int kFaceSouth = 1;
 //	public static final int kFaceEast = 2;
 //	public static final int kFaceWest = 3;
-	
-	public static final int kFaceSouth = 10;
-	public static final int kFaceNorth = 4;
 	
 //	static public final int POVup = 0;
 //	static public final int POVright = 90;
